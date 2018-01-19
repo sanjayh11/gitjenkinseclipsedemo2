ea3917d0de2b2682ca0f1e4bc175f666677fedff
sanja <sanja@192.168.1.3>
2018-01-19 20:19:11 +0530

    checkin for demo project 2- Sanjay

---
+++ GitBuildDemo2/src/GitBuildDemo2/GitBuildDemo2Test.java
@@ -0,0 +1,15 @@
+package GitBuildDemo2;
+
+public class GitBuildDemo2Test {
+
+	public GitBuildDemo2Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("testing second Git setup for Jenkins 2");
+
+	}
+
+}
